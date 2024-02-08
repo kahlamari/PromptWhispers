858d746ca207c82eab29149af27b4b7f3e4472e7
kahlamari <email@vincent-kahl.net>
2024-02-08 10:13:27 +0100

    improve ui design

--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -16,7 +16,7 @@ export default function Play() {
     }
   };
 
-  const onPromptChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onPromptChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setPrompt(event.target.value);
   };
 
@@ -32,22 +32,23 @@ export default function Play() {
   };
 
   return (
-    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900 pb-5">
+    <div className="">
+      <h1 className="pb-5 text-center text-6xl font-bold text-gray-900">
         Enter your prompt!
       </h1>
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <input
-          type="text"
+      <form onSubmit={handleSubmit} className="flex">
+        <textarea
           value={prompt}
           onChange={onPromptChange}
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          rows={2}
+          placeholder="The potato king leads an uprising"
+          className="mr-4 h-full w-auto resize-none rounded-2xl p-6 text-3xl text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
         />
         <button
           type="submit"
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="w-auto justify-center rounded-2xl bg-indigo-600 px-10 py-6 text-3xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Submit
+          Done
         </button>
       </form>
     </div>
