6c52064b3ee8fcf573d65dd317df3a9d416cce9a
kahlamari <email@vincent-kahl.net>
2024-02-22 09:35:39 +0100

    fix frontend build

--- frontend/src/types/Game.ts
+++ frontend/src/types/Game.ts
@@ -2,7 +2,7 @@ import { Turn } from "./Turn.ts";
 
 export type Game = {
   id: string;
-  turns: { [key: number]: Turn[] };
+  rounds: { [key: number]: Turn[] };
   createdAt: Date;
   gameState: "NEW" | "PROMPT_PHASE" | "IMAGE_PHASE" | "FINISHED";
 };
