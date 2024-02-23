7dc91a87da00377d04fdb9972dcadac840735113
kahlamari <email@vincent-kahl.net>
2024-02-23 12:33:02 +0100

    fix page

--- frontend/src/components/GameHistory.tsx
+++ frontend/src/components/GameHistory.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Turn } from "../types/Turn.ts";
 
 export default function GameHistory() {
   const navigate = useNavigate();
@@ -18,6 +19,14 @@ export default function GameHistory() {
     axios.delete(`/api/games/${gameId}`).then(() => getGames());
   }
 
+  const getTurns = (rounds: Turn[][]): number => {
+    if (rounds[0]) {
+      return rounds[0].length;
+    }
+
+    return 0;
+  };
+
   useEffect(() => {
     getGames();
   }, []);
@@ -30,7 +39,7 @@ export default function GameHistory() {
               Game
             </th>
             <th scope="col" className="px-6 py-3">
-              Steps
+              Turns
             </th>
             <th scope="col" className="px-6 py-3">
               Finished
@@ -53,9 +62,9 @@ export default function GameHistory() {
               >
                 {game.id}
               </th>
-              <td className="px-6 py-4">{game.rounds[0].length}</td>
+              <td className="px-6 py-4">{getTurns(game.rounds)}</td>
               <td className="px-6 py-4">
-                {game.gameState === "FINISHED" ? "Completed" : "Not finished"}
+                {game.gameState === "FINISHED" ? "Yes" : "Not finished"}
               </td>
               <td className="px-6 py-4">
                 <button onClick={() => deleteGame(game.id)}>Delete</button>
