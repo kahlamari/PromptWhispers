f7065763890be53d205d69e84198c7cb32bf0c33
kahlamari <email@vincent-kahl.net>
2024-02-26 12:19:13 +0100

    add logo and favicon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { User } from "../types/User.ts";
 import { Link } from "react-router-dom";
+import logoUrl from "./../assets/promptwhispers-logo.webp";
 
 type HeaderProps = {
   user: User;
@@ -18,10 +19,15 @@ export default function Header(props: HeaderProps) {
 
   return (
     <div className="top-0 flex h-16 w-full flex-row items-center justify-between bg-indigo-600 px-3 text-indigo-50">
-      <div className="text-2xl font-extralight">
-        <Link to="/">Prompt Whispers</Link>
+      <div className="flex h-full items-center font-extralight">
+        <Link className="mr-10 flex h-full items-center" to="/">
+          <img className="mr-3 h-12 rounded-3xl" src={logoUrl} alt="logo" />
+          <h1 className="text-2xl">Prompt Whispers</h1>
+        </Link>
+        <div className="rounded-2xl p-2 hover:bg-indigo-500">
+          <Link to="/games">Games</Link>
+        </div>
       </div>
-      <Link to="/games">Games</Link>
 
       {!props.user && (
         <button
