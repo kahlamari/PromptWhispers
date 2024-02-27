24889ae0c38d2b38c38437c663d92326d9bcb798
kahlamari <email@vincent-kahl.net>
2024-02-27 10:49:08 +0100

    fix code analysis finding

--- frontend/src/ui-components/Button.tsx
+++ frontend/src/ui-components/Button.tsx
@@ -2,10 +2,10 @@ type ButtonProps = {
   caption: string;
   onClick?: () => void;
   isDisabled?: boolean;
-  type?: "submit" | "reset" | "button" | undefined;
+  type?: "submit" | "reset" | "button";
 };
 
-export default function Button(props: ButtonProps) {
+export default function Button(props: Readonly<ButtonProps>) {
   return (
     <button
       className="w-auto justify-center rounded-2xl bg-indigo-600 px-16 py-6 text-3xl font-semibold text-indigo-50 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
