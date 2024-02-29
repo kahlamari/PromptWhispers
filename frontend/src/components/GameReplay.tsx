9f71331c2f2e8d35cd3a3e7f63e90ee3e727261c
kahlamari <email@vincent-kahl.net>
2024-02-29 13:27:26 +0100

    add loading animation to replay

--- frontend/src/components/GameReplay.tsx
+++ frontend/src/components/GameReplay.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Turn } from "../types/Turn.ts";
 import { User } from "../types/User.ts";
+import Spinner from "../ui-components/Spinner.tsx";
+import ImagePlaceholder from "../ui-components/ImagePlaceholder.tsx";
 
 export default function GameReplay() {
   const params = useParams();
@@ -11,11 +13,13 @@ export default function GameReplay() {
   const [game, setGame] = useState<Game | undefined | null>(undefined);
   const [activeRoundTab, setActiveRoundTab] = useState<number>(0);
   const [visibleCount, setVisibleCount] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const handleTabClick = (tabId: number) => {
     setActiveRoundTab(tabId);
     if (activeRoundTab !== tabId) {
       setVisibleCount(1);
+      setIsLoading(true);
     }
   };
 
@@ -29,24 +33,28 @@ export default function GameReplay() {
 
   useEffect(() => {
     if (game) {
-      const timer = setInterval(() => {
-        setVisibleCount((prevCount) => {
-          if (prevCount < game?.rounds[0].length) {
-            return prevCount + 1;
-          } else {
-            clearInterval(timer);
-            return prevCount;
-          }
-        });
-      }, 2000);
+      if (visibleCount < game.rounds[0].length) {
+        const revealTimer = setInterval(() => {
+          setIsLoading(true);
+          setVisibleCount((prevCount) => prevCount + 1);
+        }, 2200);
 
-      return () => clearInterval(timer);
+        return () => clearInterval(revealTimer);
+      }
     }
-  }, [game, activeRoundTab]);
+  }, [visibleCount, game]);
+
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(loadingTimer);
+  }, [visibleCount]);
 
   if (game?.rounds) {
     return (
-      <div className="w-full sm:w-144">
+      <div className="mx-4 w-svw sm:mx-0 sm:w-144">
         <div className="mb-4 w-full border-b border-gray-200">
           <ul className="-mb-px flex flex-wrap text-center text-sm font-medium">
             {game?.players.map((player: User, index: number) => (
@@ -68,7 +76,7 @@ export default function GameReplay() {
               key={turns[index].id}
               className={`${activeRoundTab === index ? "block" : "hidden"} flex flex-col gap-y-5`}
             >
-              {turns.slice(0, visibleCount).map((turn: Turn) => (
+              {turns.slice(0, visibleCount).map((turn: Turn, index: number) => (
                 <div key={turn.id}>
                   {turn.type === "PROMPT" ? (
                     <div className="flex items-start gap-2.5">
@@ -95,7 +103,11 @@ export default function GameReplay() {
                             </span>
                           </div>
                           <p className="text-3xl font-normal text-gray-900">
-                            {turn.content}
+                            {isLoading && index === visibleCount - 1 ? (
+                              <Spinner size="md" />
+                            ) : (
+                              turn.content
+                            )}
                           </p>
                         </div>
                       </div>
@@ -107,19 +119,23 @@ export default function GameReplay() {
                         src="/openai-logo.png"
                         alt="Profile Pic"
                       />
-                      <div className="flex flex-col gap-1">
+                      <div className="flex w-full flex-col gap-1">
                         <div className="leading-1.5 flex w-full flex-col rounded-e-2xl rounded-es-2xl border-gray-200 bg-gray-50 p-4">
                           <div className="mb-2 flex items-center space-x-2">
                             <span className="text-sm font-semibold text-gray-900">
                               Dall-E
                             </span>
                           </div>
-                          <div className="">
-                            <img
-                              src={turn.content}
-                              className="rounded-2xl"
-                              alt="generated"
-                            />
+                          <div className="aspect-square w-full items-center sm:w-128">
+                            {isLoading && index == visibleCount - 1 ? (
+                              <ImagePlaceholder />
+                            ) : (
+                              <img
+                                src={turn.content}
+                                className="rounded-2xl"
+                                alt="generated"
+                              />
+                            )}
                           </div>
                         </div>
                       </div>
